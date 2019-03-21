d904d0c79c4905cfed4f99b32647e94b1a7810ed
Wangqsooooo <3150102980@zju.edu.cn>
2019-03-21 15:00:58 +0800

    20190321

--- 20190307_616_error/20190307_616_error/main.cpp
+++ 20190307_616_error/20190307_616_error/main.cpp
@@ -1,39 +1,65 @@
 #include<iostream>
 #include<cmath>
+#include<string>
 #include"exc_mean.h"
+
+class demo
+{
+private:
+	std::string word;
+
+public:
+	demo(const std::string & str)
+	{
+		word = str;
+		std::cout << "demo " << word << " created\n";
+	}
+	~demo()
+	{
+		std::cout << "demo " << word << " destroyed\n";
+	}
+
+	void show() const
+	{
+		std::cout << "demo " << word << " lives!\n";
+	}
+};
 
 double hmean(double a, double b);
 double gmean(double a, double b);
+double means(double a, double b);
 
 int main(void)
 {
 	double x, y, z;
-
-	std::cout << "Enter two numbers: ";
-	while (std::cin >> x >> y)
 	{
-		try
+		demo d1("found in block in main()");
+
+		std::cout << "Enter two numbers: ";
+		while (std::cin >> x >> y)
 		{
-			z = hmean(x, y);
-			std::cout << "Harmonic mean of " << x << " and " << y
-				<< " is " << z << std::endl;
-			std::cout << "Geometric mean of " << x << " and " << y
-				<< " is " << gmean(x, y) << std::endl;
-			std::cout << "Enter next set of numbers <q to quit>: ";
+			try
+			{
+				z = means(x, y);
+				std::cout << "The mean mean of " << x << " and " << "y"
+					<< " is " << z << std::endl;
+				std::cout << "Enter next set of numbers <q to quit>: ";
+			}
+			catch (bad_hmean & bg)
+			{
+				bg.mesg();
+				std::cout << "Try again.\n";
+				continue;
+			}
+			catch (bad_gmean & hg)
+			{
+				std::cout << hg.mesg();
+				std::cout << "Values used: " << hg.v1 << ", " << hg.v2 << std::endl;
+				std::cout << "Sorry, you don't get to play any more.\n";
+				break;
+			}
 		}
-		catch (bad_hmean & bg)
-		{
-			bg.mesg();
-			std::cout << "Try again.\n";
-			continue;
-		}
-		catch (bad_gmean & hg)
-		{
-			std::cout << hg.mesg();
-			std::cout << "Values used: " << hg.v1 << ", " << hg.v2 << std::endl;
-			std::cout << "Sorry, you don't get to play any more.\n";
-			break;
-		}
+		d1.show();
 	}
 	if (std::cin.fail())
 	{
@@ -66,4 +92,24 @@ double gmean(double a, double b)
 	if (a < 0 || b < 0)
 		throw bad_gmean(a, b);
 	return std::sqrt(a*b);
+}
+
+double means(double a, double b)
+{
+	double am, hm, gm;
+	demo d2("found in means() ");
+	am = (a + b) / 2.0;
+	try
+	{
+		hm = hmean(a, b);
+		gm = gmean(a, b);
+	}
+	catch (bad_hmean & bg)
+	{
+		bg.mesg();
+		std::cout << "Caught in means()\n";
+		throw;
+	}
+	d2.show();
+	return (am + hm + gm) / 3.0;
 }
